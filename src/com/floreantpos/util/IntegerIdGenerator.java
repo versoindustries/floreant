91ba14ea8836b0f871eb14f1161e20ea92d4cb50
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2017-08-30 14:58:09 +0000

    id generation strategy changed

--- src/com/floreantpos/util/IntegerIdGenerator.java
+++ src/com/floreantpos/util/IntegerIdGenerator.java
@@ -3,6 +3,7 @@ package com.floreantpos.util;
 import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.sql.Timestamp;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.hibernate.HibernateException;
 import org.hibernate.engine.SessionImplementor;
@@ -13,9 +14,11 @@ import com.floreantpos.model.dao.RestaurantDAO;
 public class IntegerIdGenerator implements IdentifierGenerator {
 	private final static RestaurantDAO restaurantDAO = RestaurantDAO.getInstance();
 	private static int lastGeneratedId;
+	AtomicInteger at = new AtomicInteger();
 
 	@Override
 	public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
+		System.out.println("generating id for " + object.getClass());
 		Integer generatedId = null;
 		try {
 			Class<? extends Object> clazz = object.getClass();
@@ -42,10 +45,11 @@ public class IntegerIdGenerator implements IdentifierGenerator {
 			Timestamp timestamp = restaurantDAO.geTimestamp();
 			long time = timestamp.getTime();
 			generatedId = (int) ((time / 1000L) % Integer.MAX_VALUE);
+			//generatedId = at.incrementAndGet();
 		}
 		if (generatedId == lastGeneratedId) {
 			try {
-				Thread.sleep(1);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 			}
 			return generate(session, object);
